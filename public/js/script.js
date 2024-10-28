5260b6418243c8f2c06757f6f5d32f4d905b6295
mamplata <deejaymamplata29@gmail.com>
2024-10-28 11:49:39 +0800

    update crsf token

--- public/js/script.js
+++ public/js/script.js
@@ -1,51 +1,52 @@
-$(document).ready(function() {
-    const quotesListContainer = $('#quotesListContainer');
-    const searchForm = $('#searchForm');
-    const txtSearch = $('#txtSearch');
-    const csrfToken = $('meta[name="csrf-token"]').attr('content');
+$(document).ready(function () {
+    const quotesListContainer = $("#quotesListContainer");
+    const searchForm = $("#searchForm");
+    const txtSearch = $("#txtSearch");
+    const csrfToken = $('meta[name="csrf-token"]').attr("content");
+
+    console.log(csrfToken);
 
     const waitResponse = async (callback) => await callback();
 
     async function displayAllQuotes() {
         $.ajax({
-            url: '/quotes/all',
-            method: 'GET',
-            success: function(response) {
+            url: "/quotes/all",
+            method: "GET",
+            success: function (response) {
                 quotesListContainer.html(response);
             },
-            error: function(xhr) {
+            error: function (xhr) {
                 console.error(xhr.responseText);
-            }
+            },
         });
     }
 
     async function getQuote(keyword) {
         $.ajax({
-            url: '/quotes/search_result',
-            method: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify({
+            url: "/quotes/search_result",
+            method: "POST",
+            data: {
                 keyword: keyword,
-                _token: csrfToken
-            }),
-            success: function(response) {
+                _token: csrfToken,
+            },
+            success: function (response) {
                 quotesListContainer.html(response);
             },
-            error: function(xhr) {
+            error: function (xhr) {
                 console.error(xhr.responseText);
-            }
+            },
         });
     }
 
-    $(window).on('load', function() {
+    $(window).on("load", function () {
         waitResponse(displayAllQuotes());
     });
 
-    txtSearch.on('keyup', function(e) {
+    txtSearch.on("keyup", function (e) {
         waitResponse(getQuote(e.target.value));
     });
 
-    searchForm.on('submit', function(e) {
+    searchForm.on("submit", function (e) {
         e.preventDefault();
         waitResponse(getQuote(txtSearch.val()));
     });
